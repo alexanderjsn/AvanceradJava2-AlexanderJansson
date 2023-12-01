71aaa5dc58cd889a0e65efd35bd387d88a8746fc
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-12-01 21:09:21 +0100

    la till defaulttablemodel + första rad på table. Testade att lägga in labels och sortera alfabetiskt.

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -1,15 +1,22 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class GUI extends JFrame implements ActionListener{
     private JButton fileButton = new JButton("Choose file");
     private JFrame frame = new JFrame();
 
-    private JTable table = new JTable();
+
+    private String[] columnLabel = {"1","2","3"};
+    private DefaultTableModel tableModel = new DefaultTableModel(columnLabel, 0 );
+
+    private JTable table = new JTable(tableModel);
+
 
     public GUI() {
         setSize(1500,1500);
@@ -19,7 +26,18 @@ public class GUI extends JFrame implements ActionListener{
         fileButton.setBackground(Color.GRAY);
         add(table,BorderLayout.CENTER);
 
+        String[] text = {"B","C","A"};
 
+        for (String str : text){
+            JLabel label = new JLabel(str);
+
+            tableModel.addRow(text);
+            Arrays.sort(text);
+        }
+
+
+
+        frame.pack();
         setVisible(true);
     }
 
