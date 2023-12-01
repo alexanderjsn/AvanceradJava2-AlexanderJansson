a3b1649244246448f5186871df5870f56a5f8a9b
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-12-01 20:49:47 +0100

    skapa actionevent för knapp + la knapp på en högre nivå i scope

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -2,20 +2,28 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ActionListener{
+    private JButton fileButton = new JButton("Choose file");
     public GUI() {
         JFrame frame = new JFrame();
         frame.setSize(1500,1500);
         frame.setLayout(new BorderLayout());
-
-        JButton fileButton = new JButton("Choose file");
         frame.add(fileButton,BorderLayout.NORTH);
 
         frame.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == fileButton){
+
+        }
+    }
+
     // lägg till actionlistener
+
     // lägg till metod här
 }
