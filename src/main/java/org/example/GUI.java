2b655af90ba98e99ff252f3e79f83bf9666c4980
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-12-01 20:45:24 +0100

    skapade frame

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+    public GUI() {
+        JFrame frame = new JFrame();
+        frame.setSize(1500,1500);
+        frame.setLayout(new BorderLayout());
+
+
+
+        frame.setVisible(true);
+    }
+
+    // lägg till actionlistener
+    // lägg till metod här
 }
